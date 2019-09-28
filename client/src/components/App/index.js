471cba791d0e596cd6356b20a2e07ce1dd58e0c3
sean lee <seanboramlee@gmail.com>
2019-09-27 17:18:46 -0700

    Add App, Buttons, ImageUPload and Spinner components

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,65 @@
 import React from 'react'
-import logo from './logo.svg'
+import Spinner from '../Spinner'
+import Images from '../ImageUpload'
+import Buttons from '../Buttons'
+// import { API_URL } from './config'
+import './index.css'
 
-const App = () => {
-  return <div>hi</div>
+class App extends React.Component {
+  state = {
+    uploading: false,
+    images: [],
+  }
+
+  onChange = e => {
+    const files = Array.from(e.target.files)
+    this.setState({ uploading: true })
+
+    const formData = new FormData()
+
+    files.forEach((file, i) => {
+      formData.append(i, file)
+    })
+
+    // fetch(`${API_URL}/image-upload`, {
+    //   method: 'POST',
+    //   body: formData,
+    // })
+    //   .then(res => res.json())
+    //   .then(images => {
+    //     this.setState({
+    //       uploading: false,
+    //       images,
+    //     })
+    //   })
+  }
+
+  removeImage = id => {
+    this.setState({
+      images: this.state.images.filter(image => image.public_id !== id),
+    })
+  }
+
+  render() {
+    const { uploading, images } = this.state
+
+    const content = () => {
+      switch (true) {
+        case uploading:
+          return <Spinner />
+        case images.length > 0:
+          return <Images images={images} removeImage={this.removeImage} />
+        default:
+          return <Buttons onChange={this.onChange} />
+      }
+    }
+
+    return (
+      <div>
+        <div className="buttons">{content()}</div>
+      </div>
+    )
+  }
 }
 
 export default App
